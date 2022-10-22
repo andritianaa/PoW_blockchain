3cebe02300f2f4066ca86bc9be189fb38047920d
Rakotonimanana T. Andritiana Steve <pro@andritiana.tech>
2022-10-22 18:09:43 +0300

    code cleaned

--- API/index.js
+++ API/index.js
@@ -1,26 +1,20 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const Blockchain = require('./../blockchain/blockchain');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(bodyParser.json());
-
-const blockchain = new Blockchain();
-
+const bodyParser = require('body-parser')
+const express = require('express')
+const Blockchain = require('./../blockchain/blockchain')
+const PORT = process.env.PORT || 3001
+const app = express()
+app.use(bodyParser.json())
+const blockchain = new Blockchain()
 app.get('/blocks', (req, res) => {
-    res.status(200).json(blockchain.chain);
+    res.status(200).json(blockchain.chain)
 })
 
 app.post('/mine', (req, res) => {
- 
-    const block = blockchain.addBlock(req.body.data);
-    console.log('here');
-    console.log(`Nouveau block: ${block.toString()}`);
-    res.redirect('/blocks');
+    const block = blockchain.addBlock(req.body.data)
+    console.log(`Nouveau block: ${block.toString()}`)
+    res.redirect('/blocks')
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,41 +1,39 @@
-const Block = require('./block');
-
+const Block = require('./block')
 class Blockchain {
 
     constructor() {
-        this.chain = [Block.genesis()];
+        this.chain = [Block.genesis()]
     }
 
     addBlock(data) {
-        const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
-        this.chain.push(block);
-
-        return block;
+        const block = Block.mineBlock(this.chain[this.chain.length - 1], data)
+        this.chain.push(block)
+        return block
     }
 
     isValidChain(chain) {
 
         if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
-            return false;
+            return false
         }
         for (let i = 1; i < chain.length; i++) {
-            const block = chain[i];
-            const lastBlock = chain[i - 1];
+            const block = chain[i]
+            const lastBlock = chain[i - 1]
             if ((block.lastHash !== lastBlock.hash) || (block.hash !== Block.blockHash(block))) {
-                return false;
+                return false
             }
         }
-        return true;
+        return true
     }
 
     replaceChain(newChain) {
         if (newChain.length <= this.chain.length) {
-            console.log("Recieved chain is not longer than the current chain");
+            console.log("Recieved chain is not longer than the current chain")
         } else if (!this.isValidChain(newChain)) {
-            console.log("Recieved chain is invalid");
+            console.log("Recieved chain is invalid")
         } else {
-            console.log("Replacing the current chain with new chain");
-            this.chain = newChain;
+            console.log("Replacing the current chain with new chain")
+            this.chain = newChain
         }
     }
 }
