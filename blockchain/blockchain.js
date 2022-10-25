2ff40503e20f26a3eae8cb52d491b99c4a4a4400
Rakotonimanana T. Andritiana Steve <pro@andritiana.tech>
2022-10-25 18:48:24 +0300

    update

--- blockchain/blockchain.js
+++ blockchain/blockchain.js
@@ -1,40 +1,44 @@
-const Block = require('./block')
-class Blockchain {
+const Block = require('./block');
 
+class Blockchain {
     constructor() {
-        this.chain = [Block.genesis()]
+        this.chain = [Block.genesis()];
     }
 
     addBlock(data) {
-        const block = Block.mineBlock(this.chain[this.chain.length - 1], data)
-        this.chain.push(block)
-        return block
+        const block = Block.mineBlock(this.chain[this.chain.length - 1], data);
+        this.chain.push(block);
+
+        return block;
     }
 
     isValidChain(chain) {
+        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()))
+            return false;
 
-        if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
-            return false
-        }
         for (let i = 1; i < chain.length; i++) {
-            const block = chain[i]
-            const lastBlock = chain[i - 1]
-            if ((block.lastHash !== lastBlock.hash) || (block.hash !== Block.blockHash(block))) {
-                return false
-            }
+            const block = chain[i];
+            const lastBlock = chain[i - 1];
+            if ((block.lastHash !== lastBlock.hash) || (
+                block.hash !== Block.blockHash(block)))
+                return false;
         }
-        return true
+
+        return true;
+
     }
 
     replaceChain(newChain) {
         if (newChain.length <= this.chain.length) {
-            console.log("Recieved chain is not longer than the current chain")
+            console.log("Recieved chain is not longer than the current chain");
+            return;
         } else if (!this.isValidChain(newChain)) {
-            console.log("Recieved chain is invalid")
-        } else {
-            console.log("Replacing the current chain with new chain")
-            this.chain = newChain
+            console.log("Recieved chain is invalid");
+            return;
         }
+
+        console.log("Replacing the current chain with new chain");
+        this.chain = newChain;
     }
 }
 
