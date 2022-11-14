7ffef677dc7fc0ae68b599662116d236f93411ab
andritianaa <andritianasteve@gmail.com>
2022-11-14 22:49:20 +0300

    create a blockchain

---
+++ API/index.js
@@ -0,0 +1,26 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const Blockchain = require('./../blockchain/blockchain');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.json());
+
+const blockchain = new Blockchain();
+
+app.get('/blocks', (req, res) => {
+    res.status(200).json(blockchain.chain);
+})
+
+app.post('/mine', (req, res) => {
+ 
+    const block = blockchain.addBlock(req.body.data);
+    console.log('here');
+    console.log(`Nouveau block: ${block.toString()}`);
+    res.redirect('/blocks');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
